eaf5bf3c39302cb529e3931e51eba6ea391ad6be
Sarah Kim <kimsuyon2013@gmail.com>
2023-06-27 12:03:01 -0700

    created POST route for Board

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,4 +1,23 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
-# example_bp = Blueprint('example_bp', __name__)
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+@boards_bp.route("", methods=["POST"])
+def create_boards():
+    request_body = request.get_json()
+    new_board = Board(title=request_body["title"], 
+                      owner=request_body["owner"])
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return make_response(f"Board id:{new_board.board_id} created, 201")
+
+# @boards_bp.route("", methods=["GET"])
+# def get_all_boards():
+#     all_boards = Board.query.all()
+#     boards_response = []
+#     for board in all_boards:
+#         boards_response.append()
