6fdd4bd1e08796c0e42dcc371b8cd44b1a65385f
jasmine <jtmsoto@gmail.com>
2023-06-28 11:04:12 -0700

    add validate_item function to card_routes

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -2,8 +2,17 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.card import Card
 
-# example_bp = Blueprint('example_bp', __name__)
+
 card_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+def validate_item(model, item_id):
+    try:
+        item_id = int(item_id)
+    except ValueError:
+        return abort(make_response({"message": f"invalid id: {item_id}"}, 400))
+    
+    return model.query.get_or_404(item_id)
+
 
 @card_bp.route("", methods=["GET"])
 def get_all_cards():
@@ -32,21 +41,22 @@ def create_cards():
 
     return make_response(f"Card id:{new_card.card_id} created, 201")
 
+
 @card_bp.route("/<card_id>", methods=["DELETE"])
 def delete_card(card_id):
-    card = Card.query.get_or_404(card_id)
+    card = validate_item(Card, card_id)
 
     db.session.delete(card)
     db.session.commit()
 
     return {"details": f'Card {card_id} successfully deleted'}, 200
 
+
 @card_bp.route("/<card_id>", methods=["PATCH"])
 def update_card_likes(card_id):
+    card = validate_item(Card, card_id)
 
     request_data = request.get_json()
-    card = Card.query.get_or_404(card_id)
-
     card.likes_count = request_data["likes_count"]
 
     db.session.commit()
